5feb160c6da7557ce8f255a003d2535b2ddac254
Pranjal Deep <53447129+pranjal-deep@users.noreply.github.com>
2019-09-26 15:46:10 -0400

    adding Jquery

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function(){
+    function openNav(){
+        $("#mySideNav").addClass("openMenu");
+    }
+    
+    function closeNav(){
+        $("#mySideNav").removeClass("openMenu");
+    }
+})
